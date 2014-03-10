e5179217781feb6bebb151cb50f84a40d488632a
Jonas Juselius <jonas.juselius@uit.no>
2014-03-10 22:38:02 +0100

    Fixed master on desktop 1.

--- xmonad.hs
+++ xmonad.hs
@@ -3,23 +3,37 @@ import XMonad.Config.Desktop
 import XMonad.Hooks.ManageHelpers
 import XMonad.Config.Gnome
 import XMonad.Layout.PerWorkspace
-import XMonad.Layout.NoFrillsDecoration
 import XMonad.Layout.NoBorders
+import XMonad.Hooks.InsertPosition
 import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+
+main = xmonad $ gnomeConfig
+    { modMask            = mod4Mask
+    , layoutHook         = myLayout
+    , manageHook         = myManageHook <+> manageHook gnomeConfig
+    , startupHook        = startup
+    , keys               = myKeys <+> keys gnomeConfig
+    , borderWidth        = 3
+    , normalBorderColor  = "#444444"
+    , focusedBorderColor = "#0066aa"
+    , workspaces = map show [1..9]
+}
 
 myManageHook = composeAll [
     isFullscreen --> doFullFloat
     , (className =? "Thunderbird") --> doShift "1"
-    , (className =? "Thunderbird" <&&>
-        appName =? "Msgcompose") -->  doF W.swapDown
     , (className =? "Google-chrome") --> doShift "2"
     , (className =? "Spotify") --> doShift "2"
     , (className =? "VirtualBox") --> doShift "9" <+> doFloat
     , (className =? "Vlc") --> doFloat
-    , (className =? "Eog") --> doFloat
     , (className =? "Mplayer2") --> doCenterFloat
     , (className =? "Gnome-panel" <&&>
         title =? "Run Application") --> doCenterFloat
+    , (currentWs =? "1") --> insertPosition Below Newer
+    , (currentWs =? "2") --> insertPosition Below Newer
+        {-appName =? "Msgcompose") -->  doF W.swapDown-}
+    {-, (className =? "Eog") --> doFloat-}
 --     , (className =? "Diffuse") --> doFloat
 --     , (className =? "Pidgin" <&&>
 --          (title =? "Pidgin" <||> title =? "Accounts")) --> doCenterFloat
@@ -28,32 +42,20 @@ myManageHook = composeAll [
     ]
 
 myLayout =
-    onWorkspaces ["1"] ( desktopLayoutModifiers $
+    onWorkspaces ["1"] (desktopLayoutModifiers $
         smartBorders $ Mirror (Tall 1 (5/100) 0.55) ||| Full) $
-    onWorkspaces ["2"] ( desktopLayoutModifiers $
+    onWorkspaces ["2"] (desktopLayoutModifiers $
         smartBorders $ Full ||| Tall 1 (3/100) 0.65) $
-    onWorkspaces ["3", "4", "5", "6"] ( desktopLayoutModifiers $
+    onWorkspaces (map show [3..9]) (desktopLayoutModifiers $
         smartBorders $ Tall 1 (3/100) 0.5 ||| Full) $
     smartBorders (layoutHook gnomeConfig)
 
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList []
+    {-[ ((modm, xK_F12), xmonadPrompt defaultXPConfig)-}
+    {-, ((modm, xK_F3 ), shellPrompt  defaultXPConfig)-}
+    {-]-}
+
 startup :: X ()
 startup = do
-    spawn "xsetroot -solid '#222222'"
+    spawn "xsetroot -solid '#888888"
 --    xloadimage -onroot -fullscreen <path.to.image>
-
-main = xmonad $ gnomeConfig
-    { modMask            = mod4Mask
-    , layoutHook         = myLayout
-    , manageHook         = myManageHook <+> manageHook gnomeConfig
-    , startupHook        = startup
-    , borderWidth        = 3
-    , normalBorderColor  = "#444444"
-    , focusedBorderColor = "#0066aa"
-    , workspaces = map show [1..9]
-}
-
--- noFrills = desktopLayoutModifiers $ noFrillsDeco shrinkText defaultTheme
---     (layoutHook defaultConfig)
-
--- myLayout = onWorkspaces ["1", "2"] (desktopLayoutModifiers $ smartBorders
--- (layoutHook gnomeConfig)) noFrills

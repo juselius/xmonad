fb9fd46169e8aa2c944e627e2909f4eb3ec8a121
Jonas Juselius <jonas.juselius@itpartner.no>
2016-11-15 11:16:12 +0100

    Add battery and text based net, mem and cpu monitors.

--- taffybar.hs
+++ taffybar.hs
@@ -1,46 +1,27 @@
 import System.Taffybar
 
 import System.Taffybar.FreedesktopNotifications
-import System.Taffybar.MPRIS
 import System.Taffybar.SimpleClock
 import System.Taffybar.Systray
 import System.Taffybar.TaffyPager
-import System.Taffybar.Weather
+import System.Taffybar.Battery
+import System.Taffybar.NetMonitor
+import System.Taffybar.Text.CPUMonitor
+import System.Taffybar.Text.MemoryMonitor
 
-import System.Taffybar.Widgets.PollingBar
-import System.Taffybar.Widgets.PollingGraph
-
-import System.Information.CPU
-import System.Information.Memory
-
-memCallback = do
-  mi <- parseMeminfo
-  return [memoryUsedRatio mi]
-
-cpuCallback = do
-  (userLoad, systemLoad, totalLoad) <- cpuLoad
-  return [totalLoad, systemLoad]
-
-main = do
-  let memCfg =
-        defaultGraphConfig
-        {graphDataColors = [(1, 0, 0, 1)], graphLabel = Just "mem"}
-      cpuCfg =
-        defaultGraphConfig
-        { graphDataColors = [(0, 1, 0, 1), (1, 0, 1, 0.5)]
-        , graphLabel = Just "cpu"
-        }
-  let clock =
-       textClockNew Nothing "<span fgcolor='orange'>%a %b %_d %H:%M</span>" 1
-      pager = taffyPagerNew defaultPagerConfig
-      note = notifyAreaNew defaultNotificationConfig
-      wea = weatherNew (defaultWeatherConfig "KMSN") 10
-      mpris = mprisNew defaultMPRISConfig
-      mem = pollingGraphNew memCfg 1 memCallback
-      cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
-      tray = systrayNew
+main =
   defaultTaffybar
     defaultTaffybarConfig
-    { startWidgets = [pager, note]
-    , endWidgets = [tray,  mem, cpu, mpris, clock]
-    }
+      { startWidgets = [pager, note]
+      , endWidgets = [clock, tray, batt, mem, cpu, net]
+      }
+  where
+    clock = textClockNew Nothing
+      "<span fgcolor='orange'>%a %b %_d %H:%M</span>" 1
+    pager = taffyPagerNew defaultPagerConfig
+    note = notifyAreaNew defaultNotificationConfig
+    batt = batteryBarNew defaultBatteryConfig 5.0
+    mem = textMemoryMonitorNew "mem:$used$" 1.0
+    cpu = textCpuMonitorNew "cpu:$total$" 0.5
+    net = netMonitorNew 1.5 "eth0"
+    tray = systrayNew

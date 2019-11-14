328b38fcc8059c96b4231c31858be22d31ef3098
Jonas Juselius <jonas.juselius@itpartner.no>
2019-11-14 18:32:56 +0100

    Changes for polybar

--- xmonad.hs
+++ xmonad.hs
@@ -8,6 +8,7 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.InsertPosition
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.UrgencyHook (UrgencyHook, urgencyHook, withUrgencyHook)
 import XMonad.Util.Run (spawnPipe, safeSpawn)
 -- import System.Taffybar.Support.PagerHints (pagerHints)
 import XMonad.Layout.ResizableTile
@@ -16,7 +17,6 @@ import qualified XMonad.StackSet as W
 import qualified XMonad.Util.EZConfig as EZ
 import qualified DBus as DBus
 import qualified DBus.Client as DBus
-import XMonad.Hooks.UrgencyHook (UrgencyHook, urgencyHook, withUrgencyHook)
 import qualified XMonad.StackSet as XStack
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.NoBorders
@@ -30,29 +30,12 @@ import XMonad.Layout.Circle
 import           XMonad.Util.NamedWindows         (getName)
 import           Codec.Binary.UTF8.String         (decodeString)
 
-fg = "#f9f7dd"
-bg = "#2f2f2f"
-lightGray = "#888888"
-midGray = "#676767"
-darkGray = "#474747"
-red = "#ff5a5f"
-green  = "#86cb92"
-yellow = "#f1f0cc"
-blue = "#07a0c3"
-purple = "#a761c2"
-cyan = "#6e98a4"
-black = "#000000"
-
-roboto  = "xft:Roboto:size=12"
-gap     = 10
-topBar  = 10
-
 main = do
     -- dbus <- DBus.connectSession
     -- DBus.requestName dbus (DBus.busName_ "org.xmonad.Log")
     --   [ DBus.nameAllowReplacement, DBus.nameReplaceExisting, DBus.nameDoNotQueue ]
     dbus <- createDBusClient
-    xmproc <- spawnPipe "xmobar"
+    -- xmproc <- spawnPipe "xmobar"
     xmonad $
         docks $
         ewmh $
@@ -63,11 +46,13 @@ main = do
             , manageHook =
                   myManageHook <+> manageDocks <+> manageHook defaultConfig
             , handleEventHook = docksEventHook <+> fullscreenEventHook
+            , logHook = dynamicLogWithPP $ myLogHook dbus
             -- , logHook = polybarWorkspacesLogHook dbus
-            , logHook = dynamicLogWithPP xmobarPP
-              { ppOutput = hPutStrLn xmproc
-              , ppTitle = xmobarColor "green" "" . shorten 50
-              }
+            -- , logHook = xdynamicLogWithPP mobarPP
+            --   { ppOutput = hPutStrLn xmproc
+            --   , ppTitle = xmobarColor "green" "" . shorten 50
+            --   }
+            -- , urgencyHook = LibNotifyUrgencyHook
             , terminal = "termite"
             , keys = myKeys <+> keys defaultConfig
             , borderWidth = 1
@@ -195,21 +180,50 @@ myKeys =
         , ("M-S-l", sendMessage MirrorShrink)
         ]
 
-polybarWorkspacesLogHook :: DBus.Client -> X ()
-polybarWorkspacesLogHook dBusClient = do
-  ewmhDesktopsLogHook
-  dynamicLogWithPP $ def {
-    ppCurrent             = polybarWorkspaceFormat bg blue,
-    ppVisible             = polybarWorkspaceFormat bg yellow,
-    ppUrgent              = polybarWorkspaceFormat bg red,
-    ppHidden              = polybarWorkspaceFormat fg bg,
-    ppHiddenNoWindows     = polybarWorkspaceFormat darkGray bg,
-    ppWsSep               = "",
-    ppSep                 = "",
-    ppOrder               = \(ws:_:t:_) -> [ws, t],
-    ppTitle               = const "",
-    ppOutput              = signalToDBus dBusClient
-    }
+-- fg = "#f9f7dd"
+-- bg = "#2f2f2f"
+-- lightGray = "#888888"
+-- midGray = "#676767"
+-- darkGray = "#474747"
+-- red = "#ff5a5f"
+-- green  = "#86cb92"
+-- yellow = "#f1f0cc"
+-- blue = "#07a0c3"
+-- purple = "#a761c2"
+-- cyan = "#6e98a4"
+-- black = "#000000"
+
+-- roboto  = "xft:Roboto:size=12"
+-- gap     = 10
+-- topBar  = 10
+green     = "#b8bb26"
+darkgreen = "#98971a"
+red       = "#fb4934"
+darkred   = "#cc241d"
+yellow    = "#fabd2f"
+blue      = "#83a598"
+purple    = "#d3869b"
+aqua      = "#8ec07c"
+white     = "#eeeeee"
+
+pur2      = "#5b51c9"
+blue2     = "#2266d0"
+
+-- polybarWorkspacesLogHook :: DBus.Client -> X ()
+-- polybarWorkspacesLogHook dBusClient = do
+--   ewmhDesktopsLogHook
+--   dynamicLogWithPP $ def {
+--     ppCurrent             = polybarWorkspaceFormat bg blue,
+--     ppVisible             = polybarWorkspaceFormat bg yellow,
+--     ppUrgent              = polybarWorkspaceFormat bg red,
+--     ppHidden              = polybarWorkspaceFormat fg bg,
+--     ppHiddenNoWindows     = polybarWorkspaceFormat darkGray bg,
+--     ppWsSep               = "",
+--     ppSep                 = "",
+--     ppOrder               = \(ws:_:t:_) -> [ws, t],
+--     ppTitle               = const "",
+--     ppOutput              = signalToDBus dBusClient
+--     }
 
 data LibNotifyUrgencyHook = LibNotifyUrgencyHook deriving (Read, Show)
 instance UrgencyHook LibNotifyUrgencyHook where
@@ -218,42 +232,70 @@ instance UrgencyHook LibNotifyUrgencyHook where
     Just idx <- XStack.findTag w <$> gets windowset
     safeSpawn "notify-send" [show name, "workspace " ++ idx]
 
-polybarWorkspaceFormat :: String -> String -> WorkspaceId -> String
-polybarWorkspaceFormat foreground background workspaceId =
-  "%{F" ++ foreground ++ " B" ++ background ++ "}  " ++ icon workspaceId ++ "  %{F- B-}"
-  where
-    icon w | w == "WEB"   = "\62057"
-           | w == "EMACS" = "\61982"
-           | w == "TERM" = "\61728"
-           | w == "VID" = "\61448"
-           | w == "FILE" = "\61563"
-           | otherwise = "\62060"
+-- polybarWorkspaceFormat :: String -> String -> WorkspaceId -> String
+-- polybarWorkspaceFormat foreground background workspaceId =
+--   "%{F" ++ foreground ++ " B" ++ background ++ "}  " ++ icon workspaceId ++ "  %{F- B-}"
+--   where
+--     icon w | w == "WEB"   = "\62057"
+--            | w == "EMACS" = "\61982"
+--            | w == "TERM" = "\61728"
+--            | w == "VID" = "\61448"
+--            | w == "FILE" = "\61563"
+--            | otherwise = "\62060"
 
-dBusInterface = "user.xmonad.log"
-dBusPath = "/user/xmonad/log"
-dBusMember = "DynamicLogWithPP"
+-- dBusPath = "/user/xmonad/log"
+-- dBusMember = "DynamicLogWithPP"
+
+myAddSpaces :: Int -> String -> String
+myAddSpaces len str = sstr ++ replicate (len - length sstr) ' '
+  where
+    sstr = shorten len str
+
+myLogHook :: DBus.Client -> PP
+myLogHook dbus = def
+    { ppOutput = dbusOutput dbus
+    , ppCurrent = wrap ("%{F" ++ blue2 ++ "} ") " %{F-}"
+    , ppVisible = wrap ("%{F" ++ blue ++ "} ") " %{F-}"
+    , ppUrgent = wrap ("%{F" ++ red ++ "} ") " %{F-}"
+    , ppHidden = wrap " " " "
+    , ppWsSep = ""
+    , ppSep = " | "
+    , ppTitle = myAddSpaces 25
+    }
 
 createDBusClient :: IO DBus.Client
 createDBusClient = do
   client <- DBus.connectSession
-  DBus.requestName client (DBus.busName_ dBusInterface) dBusParams
+  DBus.requestName client (DBus.busName_ "user.xmonad.log") dBusParams
   return client
   where
     dBusParams = [
       DBus.nameAllowReplacement,
       DBus.nameReplaceExisting,
       DBus.nameDoNotQueue]
-
-signalToDBus :: DBus.Client -> String -> IO ()
-signalToDBus client message =  do
-  let signal = (DBus.signal objectPath interfaceName memberName) {
-    DBus.signalBody = [DBus.toVariant $ decodeString message]
-  }
-  DBus.emit client signal
+--
+-- Emit a DBus signal on log updates
+dbusOutput :: DBus.Client -> String -> IO ()
+dbusOutput dbus str = do
+    let signal = (DBus.signal objectPath interfaceName memberName) {
+            DBus.signalBody = [DBus.toVariant $ UTF8.decodeString str]
+        }
+    DBus.emit dbus signal
   where
-    objectPath = DBus.objectPath_ dBusPath
-    interfaceName = DBus.interfaceName_ dBusInterface
-    memberName = DBus.memberName_ dBusMember
+    objectPath = DBus.objectPath_ "/org/xmonad/Log"
+    interfaceName = DBus.interfaceName_ "org.xmonad.Log"
+    memberName = DBus.memberName_ "Update"
+
+-- signalToDBus :: DBus.Client -> String -> IO ()
+-- signalToDBus client message =  do
+--   let signal = (DBus.signal objectPath interfaceName memberName) {
+--     DBus.signalBody = [DBus.toVariant $ decodeString message]
+--   }
+--   DBus.emit client signal
+--   where
+--     objectPath = DBus.objectPath_ dBusPath
+--     interfaceName = DBus.interfaceName_ dBusInterface
+--     memberName = DBus.memberName_ dBusMember
 
 startup :: X ()
 startup = return ()
@@ -286,21 +328,6 @@ gnomeRegister2 =
                 , "string:xmonad"
                 , "string:" ++ sessionId
                 ]
-
-dbusLogHook :: DBus.Client -> PP
-dbusLogHook dbus = def { ppOutput = dbusOutput dbus }
-
--- Emit a DBus signal on log updates
-dbusOutput :: DBus.Client -> String -> IO ()
-dbusOutput dbus str = do
-    let signal = (DBus.signal objectPath interfaceName memberName) {
-            DBus.signalBody = [DBus.toVariant $ UTF8.decodeString str]
-        }
-    DBus.emit dbus signal
-  where
-    objectPath = DBus.objectPath_ "/org/xmonad/Log"
-    interfaceName = DBus.interfaceName_ "org.xmonad.Log"
-    memberName = DBus.memberName_ "Update"
 
 dmenu = --"exec dmenu_run"
     unwords
